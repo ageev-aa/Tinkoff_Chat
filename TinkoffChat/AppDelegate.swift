85f6ab079306f2db6b3f3cf4dfc576bd9b9ce283
Aleksandr <ageev-aa@yandex.ru>
2018-09-24 23:43:01 +0300

    init
    AppDelegate and ViewController methods

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -12,35 +12,61 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var logFlag = true
+    var currentState = UIApplication.shared.applicationState
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        let functionName=#function
+        getStateAndPrint(functionName)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let functionName=#function
+        getStateAndPrint(functionName)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        let functionName=#function
+        getStateAndPrint(functionName)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        let functionName=#function
+        getStateAndPrint(functionName)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        let functionName=#function
+        getStateAndPrint(functionName)
     }
-
+    
+    func getStateAndPrint(_ functionName: String){
+        if logFlag {
+            let state = UIApplication.shared.applicationState
+            if (state != currentState){
+                print("Application moved from \(printState(currentState)) to \(printState(state)) : \(functionName)")
+                currentState=state
+            } else {
+                print("Method: \(functionName), state: \(printState(state))")
+            }
+        }
+    }
+    
+    func printState (_ state: UIApplication.State) -> String {
+        switch state {
+        case .active:
+            return "active"
+        case .inactive:
+            return "inactive"
+        case .background:
+            return "background"
+        }
+    }
+    
 
 }
 
