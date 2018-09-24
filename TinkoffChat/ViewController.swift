4c4d48dfae87fcc3406e51b1f52ce7e06ef9b187
Aleksandr <aleksandr@macbook-pro-aleksandr.local>
2018-09-24 22:13:09 +0300

    Initial Commit

---
+++ TinkoffChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinkoffChat
+//
+//  Created by Aleksandr on 24.09.2018.
+//  Copyright © 2018 Aleksandr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
